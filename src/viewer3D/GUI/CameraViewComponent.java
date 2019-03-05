8f2d0d8bf21524989d181cedc90b6e6ae4e6a2b7
Arik Dicks <ariksven@gmail.com>
2019-03-04 22:46:28 -0800

    Gimbal-lock rotation implemented

--- src/viewer3D/GUI/CameraViewComponent.java
+++ src/viewer3D/GUI/CameraViewComponent.java
@@ -1,44 +1,70 @@
 package viewer3D.GUI;
 
+import java.awt.AWTException;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.Point;
 import java.awt.RenderingHints;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.Line2D;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JComponent;
 import javax.swing.border.LineBorder;
+import viewer3D.GraphicsEngine.Camera;
 import viewer3D.GraphicsEngine.Polygon;
+import static viewer3D.GraphicsEngine.Rotation.*;
 
 /**
  * A Component that is responsible for drawing a set of screen-space polygons given by a camera object
  * @author Arik Dicks
  */
 public class CameraViewComponent extends JComponent {
+    private Camera camera;
     private Polygon[] polygons;
     private java.awt.Polygon[] polygons2D;
-    private ArrayList<Line2D> lines;
     private double width;
     private double height;
     private String[] data;
     private LineBorder border;
-
+    private int mouseSensitivity;
+    private boolean wasUpdated;
+    private Robot cursorSetter;
     /**
      * Constructs a CameraViewComponent with a given width and height, and a set of screen-space polygons 
+     * @param camera
      * @param polygons A set of screen-space polygons
      * @param width The width of this component
      * @param height The height of this component
      */
-    public CameraViewComponent(Polygon[] polygons, double width, double height) {
-        //super.setFocusable(true);
+    public CameraViewComponent(Camera camera, Polygon[] polygons, double width, double height) {
+        super.setCursor(super.getToolkit().createCustomCursor(
+                   new BufferedImage( 1, 1, BufferedImage.TYPE_INT_ARGB ),
+                   new Point(),
+                   null));
+        this.camera = camera;
         this.width = width;
         this.height = height;
         this.polygons = polygons;
         polygons2D = new java.awt.Polygon[polygons.length];
-        lines = new ArrayList<>();
         border = new LineBorder(Color.BLACK, 2, false);
+        mouseSensitivity = 5;
+        try {
+            cursorSetter = new Robot();
+        } catch (AWTException ex) {}
+        //cursorSetter.mouseMove(300, 300);
+        super.addMouseMotionListener(new CursorPositionListener());
     }
+    
     /**
      * Draws the polygon faces, edges, camera data and border
      * @param g The graphics object that is drawn on
@@ -50,26 +76,21 @@ public class CameraViewComponent extends JComponent {
         
         // Draw Polygon Faces
         for (int i = 0; i < polygons2D.length; i++) {
-            //if (polygons[i].getIsVisible()) {
-                //System.out.println("test");
-                Color faceColor = polygons[i].getFaceColor();
-                if (faceColor != null) {
-                    g2.setColor(faceColor);
-                    g2.fill(polygons2D[i]);
-                }
-            //}
+            Color faceColor = polygons[i].getFaceColor();
+            if (faceColor != null) {
+                g2.setColor(faceColor);
+                g2.fill(polygons2D[i]);
+            }
         }
         
         // Draw Polygon Edges
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         for (int i = 0; i < polygons2D.length; i++) {
-            //if (polygons[i].getIsVisible()) {
-                Color edgeColor = polygons[i].getEdgeColor();
-                if (edgeColor != null) {
-                    g2.setColor(edgeColor);
-                    g2.draw(polygons2D[i]);
-                }
-            //}
+            Color edgeColor = polygons[i].getEdgeColor();
+            if (edgeColor != null) {
+                g2.setColor(edgeColor);
+                g2.draw(polygons2D[i]);
+            }
         }
         
         // Draw Camera Data
@@ -112,6 +133,74 @@ public class CameraViewComponent extends JComponent {
             polygons2D[i].addPoint((int)Math.round(x1), (int)Math.round(y1));
             polygons2D[i].addPoint((int)Math.round(x2), (int)Math.round(y2));
             polygons2D[i].addPoint((int)Math.round(x3), (int)Math.round(y3));
+        }
+    }
+    public boolean wasUpdated() {
+        if (wasUpdated) {
+            wasUpdated = false;
+            return true;
+        }
+        return false;
+    }
+    private int getAbsoluteCenterX() {
+        return super.getLocation().x + super.getWidth()/2;
+    }
+    private int getAbsoluteCenterY() {
+        return super.getLocation().y + super.getHeight()/2;
+    }
+    private int getCenterX() {
+        return super.getWidth()/2;
+    }
+    private int getCenterY() {
+        return super.getHeight()/2;
+    }
+    private int getComponentWidth() {
+        return super.getWidth();
+    }
+    private int getComponentHeight() {
+        return super.getHeight();
+    }
+    private class CursorPositionListener implements MouseMotionListener {
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            
+            int deltaX = Math.abs(e.getX()-getCenterX());
+            int deltaY = Math.abs(e.getY()-getCenterY());
+            double xRatio = deltaX/(double)getComponentWidth();
+            double yRatio = deltaY/(double)getComponentHeight();
+            int xAngle = (int)Math.round((xRatio*mouseSensitivity)*45);
+            int yAngle = (int)Math.round((yRatio*mouseSensitivity)*45);
+            int deltaTheta = 0;
+            int deltaPhi = 0;
+            
+            if (e.getX() < getCenterX()) {
+                //camera.rotate(YAW_LEFT, xAngle);
+                deltaTheta = xAngle;
+                wasUpdated = true;
+            } else if (e.getX() > getCenterX())  {
+                //camera.rotate(YAW_RIGHT, xAngle);
+                deltaTheta = -xAngle;
+                wasUpdated = true;
+            }
+            if (e.getY() < getCenterY()) {
+                //camera.rotate(PITCH_FORWARD, yAngle);
+                deltaPhi = yAngle;
+                wasUpdated = true;
+            } else if (e.getY() > getCenterY())  {
+                //camera.rotate(PITCH_BACKWARD, yAngle);
+                deltaPhi = -yAngle;
+                wasUpdated = true;
+            }
+            if (wasUpdated) {
+                camera.rotate(deltaTheta, deltaPhi);
+                cursorSetter.mouseMove(getCenterX()+5, getCenterY()+50);
+            }
         }
     }
 }
